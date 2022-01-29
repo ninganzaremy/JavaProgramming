488371da1c0a1fbe3619c4f31414a2a6f1129dd0
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-29 15:05:37 -0700

    Java Method Overloadinng

--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -6,6 +6,29 @@ public class JavaMethods {
     static int myMathMethod(int x, int y) {
         return x + y;
     }
+    // Create a checkAge() method with an integer variable called age
+    static void checkAge(int age) {
+
+        // If age is less than 18, print "access denied"
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+
+            // If age is greater than, or equal to, 18, print "access granted"
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+
+    }
+
+    //Method Overloading
+
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethod(double x, double y) {
+        return x + y;
+    }
 
     public static void main(String[] args) {
         //call th method
@@ -14,5 +37,10 @@ public class JavaMethods {
         myMethod("Anja", 31);
         int z = myMathMethod(5, 3);
         System.out.println(z);
+        checkAge(20); // Call the checkAge method and pass along an age of 20
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
     }
 }
