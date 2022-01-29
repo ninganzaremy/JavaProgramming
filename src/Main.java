e9249d66af6f133c13241e8a3f4f9e4fdadcedae
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-29 13:59:57 -0700

    java ternary operator and Switch Statements

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,24 @@ public class Main {
         System.out.println(txt.toLowerCase());   // Outputs "hello world"
         System.out.println(txt.indexOf("World")); // Outputs 6
 
+        //Short Hand If...Else (Ternary Operator)
+        int time = 20;
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
 
+        //Java Switch Statements
+        int day = 4;
+        switch (day) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+       // Outputs "Looking forward to the Weekend"
 
 
 
