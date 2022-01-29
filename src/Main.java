a19dde3dac4952a811b3d060217b3d7ae0268ca1
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-29 13:47:39 -0700

    java data types and strings Methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello Java");
+        //Java data types
+        int myNum = 5;               // integer (whole number)
+        float myFloatNum = 5.99f;    // floating point number
+        char myLetter = 'D';         // character
+        boolean myBool = true;       // boolean
+        String myText = "Hello";     // String
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+
+        //java Strings Methods
+        String txt = "Hello World";
+        System.out.println("The length of the txt string is: " + txt.length());
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());   // Outputs "hello world"
+        System.out.println(txt.indexOf("World")); // Outputs 6
+
+
+
+
+
+
     }
 
 }
