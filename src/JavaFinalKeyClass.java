25ef34902af883fc264847cd052c423071e18ae0
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-30 13:53:24 -0700

    Java Inheritance(Subclass & Superclass)

---
+++ src/JavaFinalKeyClass.java
@@ -0,0 +1,16 @@
+final class Vehicles {
+    protected String brand = "Ford";
+    public void honk() {
+        System.out.println("Tuut, tuut!");
+    }
+}
+
+//cannot inherit from final Vehicles
+//class AccessTest extends Vehicles {
+//    private String modelName = "Mustang";
+//    public static void main(String[] args) {
+//        AccessTest myFastCar = new AccessTest();
+//        myFastCar.honk();
+//        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+//    }
+//}
