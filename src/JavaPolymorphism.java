efde0ea606678fd718fe92900bddf7b5bdf2d73d
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-30 14:21:43 -0700

    JavaPolymorphism

---
+++ src/JavaPolymorphism.java
@@ -0,0 +1,32 @@
+//Polymorphism means "many forms", and it occurs when we have many classes that are related to each other by inheritance.
+//think of a superclass called Animal that has a method called animalSound(). Subclasses of Animals could be Pigs, Cats, Dogs, Birds - And they also have their own implementation of an animal sound (the pig oinks, and the cat meows, etc.):
+class Animal {
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
+
+class Dog extends Animal {
+    public void animalSound() {
+        System.out.println("The dog says: bow wow");
+    }
+}
+
+class JavaPolymorphism {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();  // Create a Animal object
+        Animal myPig = new Pig();  // Create a Pig object
+        Animal myDog = new Dog();  // Create a Dog object
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
+//Why And When To Use "Inheritance" and "Polymorphism"?
+//- It is useful for code reusability: reuse attributes and methods of an existing class when you create a new class.
