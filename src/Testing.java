b417380d324b201ab4fa962cea5b5d1b1af27b29
Remy Ninganza <rninganz@asurite.asu.edu>
2022-02-28 12:48:12 -0700

    Java Lambda Expressions

---
+++ src/Testing.java
@@ -0,0 +1,54 @@
+public class Testing {
+    // write your code here
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubstractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+
+        if (secondNumber == 0) {
+            return 0;
+        } else {
+            return firstNumber / secondNumber;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Testing calculator = new Testing();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add = " + calculator.getAdditionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("divide = " + calculator.getDivisionResult());
+    }
+}
